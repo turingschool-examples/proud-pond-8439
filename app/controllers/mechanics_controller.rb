054ce09f689e83a11ff4b3f6966587b447349b2b
Joseph Senecal <jasenecal2@gmail.com>
2022-04-11 11:39:10 -0600

    Feat: mechanic show page

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -4,4 +4,9 @@ class MechanicsController < ApplicationController
     @mechanics = Mechanic.all
   end
 
+  def show
+    @mechanic = Mechanic.find(params[:id])
+    @rides = @mechanic.rides.descending_thrill.where(open: :true)
+  end
+
 end
