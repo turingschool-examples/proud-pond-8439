fd38cc7891c8610dedd4b9602fa9873bab8fe44f
Kim G <xiaole.guo@yahoo.com>
2022-04-11 09:35:24 -0600

    mechanic index page shows all mechanic names and years experience

---
+++ spec/features/mechanics/index_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe 'mechanic index page', type: :feature do 
+
+	before :each do 
+		@josh = Mechanic.create!(name: 'Josh', years_experience: 2)
+		@sal = Mechanic.create!(name: 'Sal', years_experience: 6)
+		@kim = Mechanic.create!(name: 'Kim', years_experience: 10)
+		@sayaka = Mechanic.create!(name: 'Sayaka', years_experience: 3)
+
+	end
+
+	it 'shows all mechanics with name and experience' do 
+		visit '/mechanics'
+		save_and_open_page
+		expect(page).to have_content('All Mechanics')
+		expect(page).to have_content('Josh')
+		expect(page).to have_content('Sal')
+		expect(page).to have_content('Kim')
+		expect(page).to have_content('Sayaka')
+		expect(page).to have_content(6)
+		expect(page).to have_content(3)
+		expect(page).to have_content(10)
+		expect(page).to have_content(2)
+		
+	end
+
+
+
+	it 'shows the everage years of experience of all mechanics' do 
+
+
+	end
+
+
+
+
+
+
+
+
+
+end
