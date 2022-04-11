065e209499130e0be2a2c987d5f124fd956f0abe
Andrew Speth <speth.andrew@yahoo.com>
2022-04-11 09:15:00 -0600

    tests for relationships

---
+++ spec/models/mechanic_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Mechanic do
+  describe 'relationships' do
+    it { should have_many(:ride_mechanics) }
+    it { should have_many(:rides).through(:ride_mechanics) }
+  end
+end
