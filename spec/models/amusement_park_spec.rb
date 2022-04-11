9cdf52e7279303e7fd905a8447dec4d214ed6b55
James Harkins <jh.harkins@protonmail.com>
2022-04-11 10:31:38 -0600

    test: create model test for amusement_park model alphabetical_rides instance method

--- spec/models/amusement_park_spec.rb
+++ spec/models/amusement_park_spec.rb
@@ -13,5 +13,16 @@ RSpec.describe AmusementPark do
         expect(amusement_park.admission_cost_to_currency).to eq("50.00")
       end
     end
+
+    describe '.alphabetical_rides' do
+      it 'can order the amusement parks rides alphabetically' do
+        amusement_park = AmusementPark.create!(name: 'Hershey Park', admission_cost: 50)
+        ride_1 = amusement_park.rides.create!(name: "The Great Bear", thrill_rating: 9, open: true)
+        ride_2 = amusement_park.rides.create!(name: "Farenheit", thrill_rating: 9, open: true)
+        ride_3 = amusement_park.rides.create!(name: "Storm Runner", thrill_rating: 10, open: true)
+
+        expect(amusement_park.alphabetical_rides).to eq([ride_2, ride_3, ride_4])
+      end
+    end
   end
 end
