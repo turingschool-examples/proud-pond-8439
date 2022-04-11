4326f03ef200c24fb4cd5c64dde99b4bbe871335
Sherman Au <shermau13@gmail.com>
2022-04-11 11:27:19 -0600

    feat: relationship to Rides

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -2,6 +2,8 @@ class Mechanic < ApplicationRecord
   validates :name, presence: true
   validates :years_of_experience, presence: true, numericality: true
 
+  has_many :rides
+
   def self.average_experience
     average(:years_of_experience).round(2)
   end
