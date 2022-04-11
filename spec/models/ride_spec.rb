49e0b780537ea473513a09b2683233c0410abf3f
Zachary Imbriaco <zachary@imbriaco.com>
2022-04-11 11:10:53 -0400

    Update: Validation tests for rides model.

--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Ride do
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:thrill_rating) }
+    # Testing boolean values isn't recommended as they convert non-boolean values to boolean.
+  end
+  
   describe 'relationships' do
     it { should belong_to(:amusement_park) }
   end
