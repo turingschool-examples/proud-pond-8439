d7c86719573649f381b7ec6a108025116f242d0d
reuben <creubend@gmail.com>
2022-04-11 09:46:32 -0600

    test: Relationship between rides and mechanics set up

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -3,5 +3,7 @@ class Ride < ApplicationRecord
   validates_presence_of :thrill_rating
   validates_presence_of :open
   belongs_to :amusement_park
-  has_and_belongs_to_many :mechanics
+  has_many :ride_mechanics
+  has_many :mechanics, through: :ride_mechanics
+  
 end
