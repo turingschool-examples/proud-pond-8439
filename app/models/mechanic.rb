0a14f9f87e13edbe89418611cd76f03b5e5c0f7a
Johnny Bowman <johntbowman.production@gmail.com>
2022-04-11 09:59:49 -0600

    Model: Mechanic model finds avg age of all mechanics

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,4 +1,8 @@
 class Mechanic < ApplicationRecord
   has_many :mechanic_rides
   has_many :rides, through: :mechanic_rides
+
+  def self.avg_years_experience
+    Mechanic.average(:years_experience).round(1).to_f
+  end
 end
