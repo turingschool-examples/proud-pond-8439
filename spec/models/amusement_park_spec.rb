541ca1fddee782b83413a4d1d2fa7e9ed823aca6
Zachary Imbriaco <zachary@imbriaco.com>
2022-04-11 11:43:03 -0400

    Test: Remember to make many-to-many relationship test for relational table model.

--- spec/models/amusement_park_spec.rb
+++ spec/models/amusement_park_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe AmusementPark, type: 'model' do
+RSpec.describe AmusementPark, type: :model do
 
   describe 'validations' do
     it { should validate_presence_of(:name) }
