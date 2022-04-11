d35a8da163b442a0a2f414220181f791316b06d3
Sandisz Thieme <sandiszthieme@gmail.com>
2022-04-11 09:53:22 -0600

    create mechanics_controller and mechanics index page

--- spec/features/mechanics/index_spec.rb
+++ spec/features/mechanics/index_spec.rb
@@ -2,25 +2,25 @@ require 'rails_helper'
 
 describe 'Mechanics index page' do
   before do
-    mech_1 = Mechanic.create!(
+    @mech_1 = Mechanic.create!(
       name: 'John Mulany',
       years_experience: 25
     )
-    mech_2 = Mechanic.create!(
+    @mech_2 = Mechanic.create!(
       name: 'Jane Bologni',
       years_experience: 45
     )
 
-    visit "/mechanics/"
+    visit "/mechanics"
   end
 
   it 'displays mechanics names and ages' do
     expect(page).to have_content("All Mechanics")
-    within("#mechanic-#{mech_1.id}") do
+    within("#mechanic-#{@mech_1.id}") do
       expect(page).to have_content("John Mulany")
       expect(page).to have_content("25")
     end
-    within("#mechanic-#{mech_2.id}") do
+    within("#mechanic-#{@mech_2.id}") do
       expect(page).to have_content("Jane Bologni")
       expect(page).to have_content("45")
     end
