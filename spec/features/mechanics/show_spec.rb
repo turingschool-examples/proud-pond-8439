12b51a2039daefd4752a392cbdb4d887c6b58231
John Hennerich <john.hennerich@gmail.com>
2022-04-11 11:43:55 -0600

    Test: spec test for add ride

--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -38,3 +38,33 @@ end
 # I see their name, years of experience, and the names of rides they’re working on
 # And I only see rides that are open
 # And the rides are listed by thrill rating in descending order (most thrills first)
+
+  it 'displays a form to add a ride to a mechanic' do
+
+      expect(page).to have_content("Add ride to workload")
+      expect(page).to have_button("Add")
+
+      click_button("Add")
+      expect(current_path).to eq("/mechanics")
+  end
+# Story 3 - Add a Ride to a Mechanic
+#
+# As a user,
+# When I go to a mechanics show page
+# I see a form to add a ride to their workload
+# When I fill in that field with an id of an existing ride and hit submit
+# I’m taken back to that mechanic's show page
+# And I see the name of that newly added ride on this mechanics show page
+#
+# Ex:
+# Mechanic: Kara Smith
+# Years of Experience: 11
+#
+# Current rides they’re working on:
+# The Frog Hopper
+# Fahrenheit
+# The Kiss Raise
+#
+# Add a ride to workload:
+# Ride Id: _pretend_this_is_a_textfield_
+# Submit
