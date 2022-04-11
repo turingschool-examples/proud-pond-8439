d110a020b6c3fc36468dc31b49750d6efab69b0d
Zachary Imbriaco <zachary@imbriaco.com>
2022-04-11 12:16:54 -0400

    Feature: Average years experience shows on index page.

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -5,4 +5,8 @@ class Mechanic < ApplicationRecord
 
   has_many :mechanics_rides
   has_many :rides, through: :mechanics_rides
+
+  def self.average_experience
+    average(:years_experience)
+  end
 end
