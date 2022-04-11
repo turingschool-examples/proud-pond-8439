5c83739b486b12b58f837c22183c1035c02b0d48
Sherman Au <shermau13@gmail.com>
2022-04-11 10:33:06 -0600

    feat: ::average_expereince

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,4 +1,8 @@
 class Mechanic < ApplicationRecord
   validates :name, presence: true
   validates :years_of_experience, presence: true, numericality: true
+
+  def self.average_experience
+    average(:years_of_experience).round(2)
+  end
 end
