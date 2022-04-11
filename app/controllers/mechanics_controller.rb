c7faf91508764f05baba137eb2bbd467933014d7
Brylan Gannon <brylan.gannon112@gmail.com>
2022-04-11 10:10:11 -0600

    ModelTest: mechanic.open_rides should return only open rides

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -2,4 +2,9 @@ class MechanicsController < ApplicationController
   def index
     @mechanics = Mechanic.all
   end
+
+  def show
+    @mechanic = Mechanic.find(params[:id])
+    @rides = @mechanic.open_rides
+  end
 end
--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -7,14 +7,30 @@ RSpec.describe Mechanic do
   end
 
   before :each do
+    @park = AmusementPark.create!(name: "Elitch Gardens", admission_cost: 65)
+
     @mechanic_1 = Mechanic.create!(name:'Billy Johnson', years_experience: 26)
     @mechanic_2 = Mechanic.create!(name:'Sam Jones', years_experience: 25)
     @mechanic_3 = Mechanic.create!(name:'Francine Halls', years_experience: 15)
+
+    @ride_1 = @park.rides.create!(name: 'The Twister', thrill_rating: 8, open: true)
+    @ride_2 = @park.rides.create!(name: 'The Twister 2', thrill_rating: 10, open: false)
+    @ride_3 = @park.rides.create!(name: 'Mind Eraser', thrill_rating: 12, open: true)
+
+    MechanicRide.create!(mechanic_id: @mechanic_1.id, ride_id: @ride_1.id)
+    MechanicRide.create!(mechanic_id: @mechanic_1.id, ride_id: @ride_2.id)
+    MechanicRide.create!(mechanic_id: @mechanic_1.id, ride_id: @ride_3.id)
   end
 
   describe 'class methods' do
     it '#average_exp' do
       expect(Mechanic.average_exp).to eq(22)
+    end
+  end
+
+  describe 'instance methods' do
+    it '.open_rides' do
+      expect(@mechanic_1.open_rides).to eq([@ride_1, @ride_3])
     end
   end
 end
