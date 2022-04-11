979dd5166854f79dd7a6dcf56b2db19f5a97db89
Sherman Au <shermau13@gmail.com>
2022-04-11 10:32:48 -0600

    test: ::average_experience

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -5,4 +5,14 @@ describe Mechanic, type: :model  do
     it { should validate_presence_of(:name)}
     it { should validate_numericality_of(:years_of_experience)}
   end
+
+  describe '::average_experience' do
+    it 'finds average of all mechanics' do
+      @mechanic1 = Mechanic.create!(name: 'Sherman', years_of_experience: 3)
+      @mechanic3 = Mechanic.create!(name: 'Scott', years_of_experience: 16)
+      @mechanic2 = Mechanic.create!(name: 'Susan', years_of_experience: 6)
+
+      expect(Mechanic.average_experience).to eq(8.33)
+    end
+  end
 end
