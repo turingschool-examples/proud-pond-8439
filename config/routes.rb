abfcacc462d0c87129f2858e96bed2b1137ebf3f
Sandisz Thieme <sandiszthieme@gmail.com>
2022-04-11 11:35:10 -0600

    Create amusement park show page

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get '/mechanics', to: 'mechanics#index'
   get '/mechanics/:id', to: 'mechanics#show'
   post '/mechanics/:mechanic_id', to: 'ride_mechanics#create'
+
+  get '/amusement_parks/:id', to: 'amusement_parks#show'
 end
