d23262779b218162c44c2af82874a8e1db4cfade
Chris Romero <chrisleeromero1@gmail.com>
2022-04-11 11:22:11 -0600

    feat: complete user story 3

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -10,4 +10,16 @@ class MechanicsController < ApplicationController
     @open_rides = @mechanic.rides.where(open: true).order(thrill_rating: :desc)
     # require 'pry'; binding.pry
   end
+
+  def new
+    @mechanic = Mechanic.find(params[:id])
+    new_ride = Ride.find(params[:ride_id])
+    # require 'pry'; binding.pry
+    @mechanic.rides.create!(name: new_ride.name,
+                            amusement_park_id: new_ride.amusement_park_id,
+                            thrill_rating: new_ride. thrill_rating,
+                            open: new_ride.open)
+    @mechanic.save
+    redirect_to "/mechanics/#{@mechanic.id}"
+  end
 end
--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -1,13 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe 'mechanics show page', type: :feature do
-  # Story 2 - Mechanic Show Page
-  #
-  # As a user,
-  # When I visit a mechanic show page
-  # I see their name, years of experience, and the names of rides they’re working on
-  # And I only see rides that are open
-  # And the rides are listed by thrill rating in descending order (most thrills first)
 
   it 'displays the name, years_experience and rides for mechanic' do
     john = Mechanic.create!(name: 'John', years_experience: 5)
@@ -19,12 +12,45 @@ RSpec.describe 'mechanics show page', type: :feature do
     visit "/mechanics/#{john.id}"
     expect(page).to have_content("John")
     expect(page).to have_content("5")
-    #  order
     expect(page).to have_content("Coaster")
     expect(page).to have_content("Tower")
     expect(page).to have_content("Water Slide")
-    save_and_open_page
     expect(page).to_not have_content("Bungee")
   end
-
+  #
+  # Story 3 - Add a Ride to a Mechanic
+  #
+  # As a user,
+  # When I go to a mechanics show page
+  # I see a form to add a ride to their workload
+  # When I fill in that field with an id of an existing ride and hit submit
+  # I’m taken back to that mechanic's show page
+  # And I see the name of that newly added ride on this mechanics show page
+  #
+  # Ex:
+  # Mechanic: Kara Smith
+  # Years of Experience: 11
+  #
+  # Current rides they’re working on:
+  # The Frog Hopper
+  # Fahrenheit
+  # The Kiss Raise
+  #
+  # Add a ride to workload:
+  # Ride Id: _pretend_this_is_a_textfield_
+  # Submit
+  it 'allows the user to add a ride to a mechanic' do
+    john = Mechanic.create!(name: 'John', years_experience: 5)
+    six_flags = AmusementPark.create!(name: 'Six Flags', admission_cost: 75)
+    coaster = john.rides.create!(name: 'Coaster', thrill_rating: 5, open: true, amusement_park_id: six_flags.id)
+    water_slide = john.rides.create!(name: 'Water Slide', thrill_rating: 3, open: true, amusement_park_id: six_flags.id)
+    tower = john.rides.create!(name: 'Tower', thrill_rating: 1, open: true, amusement_park_id: six_flags.id)
+    bungee = john.rides.create!(name: 'Bungee', thrill_rating: 5, open: false, amusement_park_id: six_flags.id)
+    twister = six_flags.rides.create!(name: 'Twister', thrill_rating: 2, open: true)
+    visit "/mechanics/#{john.id}"
+    fill_in "ride_id", with: twister.id
+    click_on "Submit"
+    expect(current_path).to eq("/mechanics/#{john.id}")
+    expect(page).to have_content("Twister")
+  end
 end
