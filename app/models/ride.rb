f1bb5d10f0b74a2169a098fb2e82b776ca8ee161
Sherman Au <shermau13@gmail.com>
2022-04-11 09:32:01 -0600

    feat: validations of attributes

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,3 +1,7 @@
 class Ride < ApplicationRecord
   belongs_to :amusement_park
+
+  validates :name, presence: true
+  validates :thrill_rating, presence: true, numericality: true
+  validates :open, inclusion: [true, false]
 end
