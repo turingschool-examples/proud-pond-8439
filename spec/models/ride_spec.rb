53bd22688d5b53493029c4468a4fcaa665ed6441
Susan Boyd <sueboyd922@gmail.com>
2022-04-11 13:29:27 -0400

    Change test for boolean validations on ride_spec

--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -8,7 +8,9 @@ RSpec.describe Ride do
   describe 'validations' do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:thrill_rating) }
-    # it { should validate_presence_of(:open) }
+    it { should allow_value(true).for :open}
+    it { should allow_value(false).for :open}
+    it { should_not allow_value(nil).for :open}
   end
 
   describe 'class methods' do
