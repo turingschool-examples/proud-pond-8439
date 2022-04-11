8d22168ac8b32e9e1a0baf0ba153e0c612f88e88
alex randolph <alexgeorgerandolph@gmail.com>
2022-04-11 09:58:05 -0600

    Test: add model test for mechanic class method all_avg_exp

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -9,6 +9,24 @@ RSpec.describe Mechanic, type: :model do
 
   end
 
+  describe 'methods' do
+    before do
+      @bob = Mechanic.create!(name: 'bob', years_experience: 15)
+      @joe = Mechanic.create!(name: 'joe', years_experience: 8)
+      @skeeter = Mechanic.create!(name: 'skeeter', years_experience: 4)
+      @chad = Mechanic.create!(name: 'chad', years_experience: 26)
+    end
+
+    it 'retruns the average experience of all mechanics' do
+
+      expect(Mechanic.all_average_exp).to eq(13.25)
+    end
+
+
+
+
+  end
+
 
 
 
