d713378c575b7b9b6925c3de4b3ee50da90090cd
Will Medders <wmedders@gmail.com>
2022-04-11 13:26:53 -0400

    Test model relationships Ride

--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe Ride do
   describe 'relationships' do
     it { should belong_to(:amusement_park) }
+    it { should have_many(:mechanic_rides) }
+    it { should have_many(:mechanics).through(:mechanic_rides) }
   end
 
   describe 'class methods' do
