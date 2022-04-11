4389b7dc7d72f958f4079f6dfb840c4055e27d21
Chris Romero <chrisleeromero1@gmail.com>
2022-04-11 10:39:04 -0600

    test: add test for user story 2

--- spec/features/mechanics/index_spec.rb
+++ spec/features/mechanics/index_spec.rb
@@ -2,16 +2,6 @@ require 'rails_helper'
 
 RSpec.describe 'mechanics index page', type: :feature do
 
-  # Story 1 - Mechanic Index Page
-
-  # As a user,
-  # When I visit the mechanics index page
-  # I see a header saying “All Mechanics”
-
-  # And I see a list of all mechanic’s names and their years of experience
-
-  # And I see the average years of experience across all mechanics
-
   it 'displays a list of names and years_experienece, and average years_experience' do
     john = Mechanic.create!(name: 'John', years_experience: 5)
     amy = Mechanic.create!(name: 'Amy', years_experience: 3)
@@ -30,14 +20,5 @@ RSpec.describe 'mechanics index page', type: :feature do
     expect(page).to have_content("10")
     expect(page).to have_content("Average Years of Experience: 5")
   end
-
-  # xit 'displays the average age of experience across all mechanics' do
-  #   john = Mechanic.create!(name: 'John', years_experience: 5)
-  #   amy = Mechanic.create!(name: 'Amy', years_experience: 5)
-  #   sally = Mechanic.create!(name: 'Sally', years_experience: 5)
-  #   sam = Mechanic.create!(name: 'Sam', years_experience: 5)
-  #   visit '/mechanics'
-  #
-  # end
 
 end
---
+++ spec/features/mechanics/show_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe 'mechanics show page', type: :feature do
+  # Story 2 - Mechanic Show Page
+  #
+  # As a user,
+  # When I visit a mechanic show page
+  # I see their name, years of experience, and the names of rides they’re working on
+  # And I only see rides that are open
+  # And the rides are listed by thrill rating in descending order (most thrills first)
+
+  it 'displays the name, years_experience and rides for mechanic' do
+    john = Mechanic.create!(name: 'John', years_experience: 5)
+    six_flags = AmusementPark.create!(name: 'Six Flags', admission_cost: 75)
+    coaster = john.rides.create!(name: 'Coaster', thrill_rating: 5, open: true, amusement_park_id: six_flags.id)
+    water_slide = john.rides.create!(name: 'Water Slide', thrill_rating: 3, open: true, amusement_park_id: six_flags.id)
+    tower = john.rides.create!(name: 'Tower', thrill_rating: 1, open: true, amusement_park_id: six_flags.id)
+    bungee = john.rides.create!(name: 'Bungee', thrill_rating: 5, open: false, amusement_park_id: six_flags.id)
+    visit "/mechanics/#{john.id}"
+    expecte(page).to have_content("John")
+    expecte(page).to have_content("5")
+    #  order
+    expecte(page).to have_content("Coaster")
+    expecte(page).to have_content("Tower")
+    expecte(page).to have_content("Water Slide")
+  end
+
+end
