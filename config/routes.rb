11cd08107043d7bc6d968bde1b93bdcb359f13e3
Sandisz Thieme <sandiszthieme@gmail.com>
2022-04-11 10:20:35 -0600

    Create mechanic show page with mech name and YOE

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/mechanics', to: 'mechanics#index'
+  get '/mechanics/:id', to: 'mechanics#show'
 end
