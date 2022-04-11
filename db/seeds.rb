b4c1b3ab979e1b73f006f5e98b19f30c827f3827
Zachary Imbriaco <zachary@imbriaco.com>
2022-04-11 11:18:23 -0400

    Migrations: CreateMechanics and CreateJoinTableRidesMechanics migrations completed and tables created.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,16 @@
 AmusementPark.destroy_all
 Ride.destroy_all
+Mechanic.destroy_all
 
 six_flags = AmusementPark.create!(name: 'Six Flags', admission_cost: 75)
 universal = AmusementPark.create!(name: 'Universal Studios', admission_cost: 80)
 
 hurler = six_flags.rides.create!(name: 'The Hurler', thrill_rating: 7, open: true)
-hurler = six_flags.rides.create!(name: 'The Scrambler', thrill_rating: 4, open: true)
-hurler = six_flags.rides.create!(name: 'Ferris Wheel', thrill_rating: 7, open: false)
+scrambler = six_flags.rides.create!(name: 'The Scrambler', thrill_rating: 4, open: true)
+ferris_wheel = six_flags.rides.create!(name: 'Ferris Wheel', thrill_rating: 7, open: false)
 
 jaws = universal.rides.create!(name: 'Jaws', thrill_rating: 5, open: true)
+
+zel = Mechanic.create!(name: "Zel Imbriaco", years_experience: 5)
+noah = Mechanic.create!(name: "Noah Imbriaco", years_experience: 3)
+
