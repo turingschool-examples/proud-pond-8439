91a9762b69c72fecf6cdd452917ac7a1046d63ae
Sherman Au <shermau13@gmail.com>
2022-04-11 09:29:46 -0600

    test: validations of attributes

--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -1,7 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe Ride do
+RSpec.describe Ride, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:name)}
+    it { should validate_numericality_of(:thrill_rating)}
+    it { should allow_value([true, false]).for(:open) }
+    it { should_not allow_value(nil).for(:open) }
+  end
+
   describe 'relationships' do
     it { should belong_to(:amusement_park) }
   end
+
+
 end
