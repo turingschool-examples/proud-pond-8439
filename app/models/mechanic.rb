e07d8aeef976d8686a07563cee1e575367cb9003
Andrew Speth <speth.andrew@yahoo.com>
2022-04-11 09:35:53 -0600

    feat: mechanics index page has header, names, years of experience for each mechanic, and average years of experience for all mechanics

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,4 +1,8 @@
 class Mechanic < ApplicationRecord
   has_many :ride_mechanics
   has_many :rides, through: :ride_mechanics
+
+  def self.average_years_experience
+    average(:years_experience)
+  end
 end
--- spec/features/mechanics/index_spec.rb
+++ spec/features/mechanics/index_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "mechanics index page" do
   it 'has a header, names and years of experience for all mechanics, and average years of experience across mechanics' do
     mech_1 = Mechanic.create!(name: "Jimmy", years_experience: 1)
     mech_2 = Mechanic.create!(name: "Barb", years_experience: 2)
-    mech_1 = Mechanic.create!(name: "Tom", years_experience: 3)
+    mech_3 = Mechanic.create!(name: "Tom", years_experience: 3)
 
     visit "/mechanics"
     expect(page).to have_content("All Mechanics")
