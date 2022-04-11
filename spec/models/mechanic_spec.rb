639d2ff3a9f6c698cac5e202fc2095718e99a46a
Alicia Watt <alicia.watt.aw@gmail.com>
2022-04-11 09:53:46 -0600

    model tests and passing tests

---
+++ spec/models/mechanic_spec.rb
@@ -0,0 +1,7 @@
+require "rails_helper"
+RSpec.describe Mechanic, type: :model do
+  describe "associations" do
+    it { should have_many :ride_mechanics }
+    it { should have_many(:rides).through(:ride_mechanics) }
+  end
+end
--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -1,7 +1,9 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Ride do
-  describe 'relationships' do
+  describe "relationships" do
     it { should belong_to(:amusement_park) }
+    it { should have_many(:ride_mechanics) }
+    it { should have_many(:mechanics).through(:ride_mechanics) }
   end
 end
