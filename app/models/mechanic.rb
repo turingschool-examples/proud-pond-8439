eee65953aaa506afc8c4591ecde24bd2a6f946ed
alex randolph <alexgeorgerandolph@gmail.com>
2022-04-11 09:59:58 -0600

    Feat: add class method to mechanic model all_avg_exp to return the average years expirience for all mechanics

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -3,4 +3,8 @@ class Mechanic < ApplicationRecord
   has_many :mechanic_rides
   has_many :rides, through: :mechanic_rides
 
+  def self.all_average_exp
+    average(:years_experience).to_f
+  end
+
 end
