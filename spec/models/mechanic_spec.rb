19f988fcf51457c25b94dc7a06fbc6bba42d7dc1
Sherman Au <shermau13@gmail.com>
2022-04-11 09:35:56 -0600

    refactor: validate numericality of years of exp

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 describe Mechanic, type: :model  do
   describe 'validations' do
     it { should validate_presence_of(:name)}
-    it { should validate_presence_of(:years_of_experience)}
+    it { should validate_numericality_of(:years_of_experience)}
   end
 end
