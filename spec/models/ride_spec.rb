c1cde5501f68b73750fa00e418e2e58ace49882c
James Harkins <jh.harkins@protonmail.com>
2022-04-11 09:13:40 -0600

    feat: pass model tests for many-to-many relationship between rides and mechanics

--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe Ride do
   describe 'relationships' do
     it { should belong_to(:amusement_park) }
     it { should have_many :ride_mechanics }
-    it { should have_many :mechanics, through: :ride_mechanics }
+    it { should have_many(:mechanics).through(:ride_mechanics) }
   end
 end
