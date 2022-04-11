24eb6dd9f7db15119ce8b5fb013200399d0fb8db
Johnny Bowman <johntbowman.production@gmail.com>
2022-04-11 10:12:00 -0600

    Test: Mechanic show page displays names of rides they're working on

--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -11,9 +11,12 @@ RSpec.describe "Mechanics show" do
 
     @jaws = @universal.rides.create!(name: 'Jaws', thrill_rating: 5, open: true)
 
-    @mech_1 = @hurler.mechanics.create!(name: "Billy Bighammer", years_experience: 50)
-    @mech_2 = @hurler.mechanics.create!(name: "Linda Licehead", years_experience: 8)
-    @mech_3 = @scrambler.mechanics.create!(name: "Rhonda Roundhouse", years_experience: 19)
+    @mech_1 = Mechanic.create!(name: "Billy Bighammer", years_experience: 50)
+    @mech_2 = Mechanic.create!(name: "Linda Licehead", years_experience: 8)
+    @mech_3 = Mechanic.create!(name: "Rhonda Roundhouse", years_experience: 19)
+
+    @hurler.mechanics << @mech_1
+    @scrambler.mechanics << @mech_1
   end
 
   it "displays mechanic name and years of experience" do
@@ -21,5 +24,17 @@ RSpec.describe "Mechanics show" do
 
     expect(page).to have_content("Name: Billy Bighammer")
     expect(page).to have_content("Years Experience: 50")
+
+    expect(page).to_not have_content("Linda Licehead")
+  end
+
+  it "displays names of rides mechanic is working on" do
+    visit "/mechanics/#{@mech_1.id}"
+
+    expect(page).to have_content("The Hurler")
+    expect(page).to have_content("The Scrambler")
+
+    expect(page).to_not have_content("Ferris Wheel")
+    expect(page).to_not have_content("Jaws")
   end
 end
