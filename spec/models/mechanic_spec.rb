f571c626581ccbc8b7a9076cd5d890f0bca5652b
Sandisz Thieme <sandiszthieme@gmail.com>
2022-04-11 10:05:58 -0600

    Test: Model Mechanic class method #average_yoe calculates average years of exp

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -10,4 +10,21 @@ describe Mechanic do
     it { should validate_presence_of(:name) }
     it { should validate_numericality_of(:years_experience) }
   end
+
+  before do
+    @mech_1 = Mechanic.create!(
+      name: 'John Mulany',
+      years_experience: 25
+    )
+    @mech_2 = Mechanic.create!(
+      name: 'Jane Bologni',
+      years_experience: 45
+    )
+  end
+
+  describe 'class methods' do
+    it 'calculates the average age of all mechanics' do
+      expect(Mechanic.average_yoe).to eq(35)
+    end
+  end
 end
