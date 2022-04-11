8b8e9371c2a038d24c6be625d0d32f32254ca30b
alex randolph <alexgeorgerandolph@gmail.com>
2022-04-11 09:29:37 -0600

    Feat: add mechanic and mechanic-ride relationship to ride model

--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -5,6 +5,6 @@ RSpec.describe Ride do
     it { should belong_to(:amusement_park) }
 
     it { should have_many :mechanic_rides }
-    it { should have_many(:mechanics).though(:mechanic_rides) }
+    it { should have_many(:mechanics).through(:mechanic_rides) }
   end
 end
