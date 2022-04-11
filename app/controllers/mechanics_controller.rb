00283a81b6042c5ea96ceb1f8536f5168f0a9b6b
Johnny Bowman <johntbowman.production@gmail.com>
2022-04-11 10:07:54 -0600

    Feat: Mechanic show page displays name and years experience

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -3,4 +3,8 @@ class MechanicsController < ApplicationController
     @mechanics = Mechanic.all
     @avg_experience = Mechanic.avg_years_experience.round(1).to_f
   end
+
+  def show
+    @mechanic = Mechanic.find(params[:id])
+  end
 end
