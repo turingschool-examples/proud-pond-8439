4e42caa57fe85861ed0991f99bdfbae936703db6
Antonio Salmeron <mr.tonyboi@gmail.com>
2022-04-11 09:20:21 -0700

    Add: Mechainc show page (UserStory 2)

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -4,4 +4,9 @@ class MechanicsController < ApplicationController
     @average_experience = Mechanic.average(:years_experience)
   end
 
+  def show
+    @mechanic = Mechanic.find(params[:id])
+    @rides = @mechanic.rides.order(thrill_rating: :desc)
+  end
+
 end
