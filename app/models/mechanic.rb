7941653814bdef0063359ae4ce8595fb5d0eab17
Andrew Speth <speth.andrew@yahoo.com>
2022-04-11 10:01:41 -0600

    feat: mechanic show page has name, years of experience, open rides they're working on, and rides are listed with highest thrill rating first

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -5,4 +5,8 @@ class Mechanic < ApplicationRecord
   def self.average_years_experience
     average(:years_experience)
   end
+
+  def open_rides
+    rides.where(open: true).order(thrill_rating: :desc)
+  end
 end
