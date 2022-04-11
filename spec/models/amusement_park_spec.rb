f558a0853d14487a2d6d2a169619609b0b709bfd
Zachary Imbriaco <zachary@imbriaco.com>
2022-04-11 11:06:12 -0400

    Update: Update model test for amusement_parks.

--- spec/models/amusement_park_spec.rb
+++ spec/models/amusement_park_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe AmusementPark do
+RSpec.describe AmusementPark, type: 'model' do
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:admission_cost) }
+  end
+
   describe 'relationships' do
     it { should have_many(:rides) }
   end
