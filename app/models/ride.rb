6ce9cf4cfa56f453cb8d954a5452300f86c3afa6
Joseph Senecal <jasenecal2@gmail.com>
2022-04-11 11:29:31 -0600

    Add model method to ride for reverse thrill ordering

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -2,4 +2,9 @@ class Ride < ApplicationRecord
   belongs_to :amusement_park
   has_many :mechanic_rides
   has_many :rides, through: :mechanic_rides
+
+  def self.descending_thrill
+    order(thrill_rating: :desc)
+  end
+  
 end
--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -4,4 +4,19 @@ RSpec.describe Ride do
   describe 'relationships' do
     it { should belong_to(:amusement_park) }
   end
+
+  describe 'methods' do
+
+    it 'returns a collection in descending order of thrill' do
+      park = AmusementPark.create!
+      ride_1 = park.rides.create!(name: "Silly Swings", thrill_rating: 2, open: true)
+      ride_2 = park.rides.create!(name: "Billy's Pirate Adventure", thrill_rating: 7, open: true)
+      ride_3 = park.rides.create!(name: "The DOOM Tower", thrill_rating: 8, open: false)
+      ride_4 = park.rides.create!(name: "Dizzy Dishes", thrill_rating: 4, open: true)
+
+      expect(Ride.descending_thrill.first).to eq(ride_3)
+      expect(Ride.descending_thrill.last).to eq(ride_1)
+
+    end
+  end
 end
