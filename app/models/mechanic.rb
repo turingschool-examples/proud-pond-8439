a8eb1550ac14855774141eb5b950b3d3a916ef57
Sandisz Thieme <sandiszthieme@gmail.com>
2022-04-11 10:06:40 -0600

    Create model method to calculate mechanic average YOE and implement on mech index page

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -4,4 +4,8 @@ class Mechanic < ApplicationRecord
 
   validates :name, presence: true
   validates :years_experience, numericality: true
+
+  def self.average_yoe
+    average(:years_experience).to_i
+  end
 end
