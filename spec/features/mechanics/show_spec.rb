5eb95cff6e98c8f6dea95da508478ae078d1f2cf
alex randolph <alexgeorgerandolph@gmail.com>
2022-04-11 10:39:38 -0600

    Feat: add form and route for user story 3: adding a ride to a mechanics work load

--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -78,11 +78,13 @@ RSpec.describe "Mechanic Show Page" do
 
       expect(page).to have_content("Add Ride to Workload:")
 
-      fill_in "Ride ID Number", with: "#{@cups.id}"
+
+
+      fill_in "Ride id number", with: "#{@cups.id}"
       click_on "Submit"
 
       expect(current_path).to eq("/mechanics/#{@chad.id}")
-      
+
       within "#rides-#{@cups.id}" do
         expect(page).to_not have_content("#{@eraser.name}")
         expect(page).to have_content("Name: Tee Cups")
