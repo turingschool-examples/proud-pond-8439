0b3cb9f384fb1a5cc8678a02313aad2700a487b5
Johnny Bowman <johntbowman.production@gmail.com>
2022-04-11 09:50:18 -0600

    Feat: Mechanic index shows names, years_experience

--- spec/features/mechanics/index_spec.rb
+++ spec/features/mechanics/index_spec.rb
@@ -5,30 +5,31 @@ RSpec.describe "Mechanics index" do
     @six_flags = AmusementPark.create!(name: 'Six Flags', admission_cost: 75)
     @universal = AmusementPark.create!(name: 'Universal Studios', admission_cost: 80)
 
-    @hurler = six_flags.rides.create!(name: 'The Hurler', thrill_rating: 7, open: true)
-    @scrambler = six_flags.rides.create!(name: 'The Scrambler', thrill_rating: 4, open: true)
-    @ferris_wheel = six_flags.rides.create!(name: 'Ferris Wheel', thrill_rating: 7, open: false)
+    @hurler = @six_flags.rides.create!(name: 'The Hurler', thrill_rating: 7, open: true)
+    @scrambler = @six_flags.rides.create!(name: 'The Scrambler', thrill_rating: 4, open: true)
+    @ferris_wheel = @six_flags.rides.create!(name: 'Ferris Wheel', thrill_rating: 7, open: false)
 
-    @jaws = universal.rides.create!(name: 'Jaws', thrill_rating: 5, open: true)
+    @jaws = @universal.rides.create!(name: 'Jaws', thrill_rating: 5, open: true)
 
-    @mech_1 = @hurler.mechanics.create!(name: "Billy Bighammer", yeras_experience: 50)
-    @mech_2 = @hurler.mechanics.create!(name: "Linda Licehead", yeras_experience: 8)
-    @mech_3 = @scrambler.mechanics.create!(name: "Rhonda Roundhouse", yeras_experience: 19)
+    @mech_1 = @hurler.mechanics.create!(name: "Billy Bighammer", years_experience: 50)
+    @mech_2 = @hurler.mechanics.create!(name: "Linda Licehead", years_experience: 8)
+    @mech_3 = @scrambler.mechanics.create!(name: "Rhonda Roundhouse", years_experience: 19)
   end
 
   it "lists all mechanic names & their years_experience" do
     visit "/mechanics"
+    save_and_open_page
 
     expect(page).to have_content("All Mechanics")
-    expect(page).to have_content("Billy Bighammer\nYears of Experience: 50")
-    expect(page).to have_content("Linda Licehead\nYears of Experience: 8")
-    expect(page).to have_content("Rhonda Roundhouse\nYears of Experience: 19")
-    expect(page).to have_content("Rhonda Roundhouse\nYears of Experience: 19")
+    expect(page).to have_content("Billy Bighammer\nYears Experience: 50")
+    expect(page).to have_content("Linda Licehead\nYears Experience: 8")
+    expect(page).to have_content("Rhonda Roundhouse\nYears Experience: 19")
+    expect(page).to have_content("Rhonda Roundhouse\nYears Experience: 19")
   end
 
-  it "lists avg years_experience for all mechanics" do
+  xit "lists avg years_experience for all mechanics" do
     visit "/mechanics"
 
-    expect(page).to have_content("Average Experience: 25.7")
+    expect(page).to have_content("Average Years Experience: 25.7")
   end
 end
