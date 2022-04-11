48b155821e72b4e69bd9ea38e74d2fa123a5cb81
Will Medders <wmedders@gmail.com>
2022-04-11 13:46:16 -0400

    Refactor model ride_spec

--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -8,13 +8,14 @@ RSpec.describe Ride do
   end
 
   describe 'class methods' do
-    Ride.destroy_all
-    AmusementPark.destroy_all
-    six_flags = AmusementPark.create!(name: 'Six Flags', admission_cost: 75)
-    hurler = six_flags.rides.create!(name: 'The Hurler', thrill_rating: 7, open: true)
-    scrambler = six_flags.rides.create!(name: 'The Scrambler', thrill_rating: 10, open: true)
-    ferris = six_flags.rides.create!(name: 'Ferris Wheel', thrill_rating: 2, open: false)
     it '#order_by_thrill' do
+      Ride.destroy_all
+      AmusementPark.destroy_all
+      six_flags = AmusementPark.create!(name: 'Six Flags', admission_cost: 75)
+      hurler = six_flags.rides.create!(name: 'The Hurler', thrill_rating: 7, open: true)
+      scrambler = six_flags.rides.create!(name: 'The Scrambler', thrill_rating: 10, open: true)
+      ferris = six_flags.rides.create!(name: 'Ferris Wheel', thrill_rating: 2, open: false)
+
       expect(Ride.order_by_thrill).to eq([scrambler, hurler, ferris])
     end
   end
