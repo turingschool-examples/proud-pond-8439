d0e7253e2a7655723ef4f5026fa24e469d5ab362
Zachary Imbriaco <zachary@imbriaco.com>
2022-04-11 13:50:28 -0400

    Refactor functionality into mechanic model for open rides sorted by thrill for an individual mechanic.

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -9,4 +9,8 @@ class Mechanic < ApplicationRecord
   def self.average_experience
     average(:years_experience)
   end
+
+  def open_rides_by_thrill
+    rides.where(open: true).order(thrill_rating: :desc)
+  end
 end
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -7,6 +7,4 @@ class Ride < ApplicationRecord
   has_many :mechanics_rides
   has_many :mechanics, through: :mechanics_rides
 
-
-
 end
