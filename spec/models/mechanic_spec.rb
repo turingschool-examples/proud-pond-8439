7709b9efb7f27c6e912309067f01e1c51896e8c0
Chris Kjolhede <ckjolhede@gmail.com>
2022-04-11 10:35:21 -0600

    write user story 1 tests

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -9,4 +9,26 @@ RSpec.describe Mechanic do
     it {should validate_presence_of(:name)}
     it {should validate_presence_of(:years_experience)}
   end
+
+  describe 'mechanic index page' do
+    it 'has header All Mechanics' do
+      visit "/mechanics/index.html.erb"
+      expect(page).to have_content("All Mechanics")
+    end
+
+    it 'has a list of all mechanics names with years exp' do
+      visit "mechanics/index.html.erb"
+        mechanics = Mechanic.all
+        mechanics.each do |mechanic|
+          expect(page).to have_content(mechanic.name)
+          expect(page).to have_content(mechanic.years_experence)
+        end
+    end
+
+    it 'shows average years exp of all mechanics' do
+      visit "mechanics/index.html.erb"
+      expect(page).to have_content(Mechanic.ave_years_experience)
+    end
+  end
+
 end
