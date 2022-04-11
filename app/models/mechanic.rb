e62cc58c74ecbf1cf0f7a3d02d277602544bf03a
reuben <creubend@gmail.com>
2022-04-11 10:02:39 -0600

    test: All Model tests pass for validations and relationships

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,4 +1,6 @@
 class Mechanic < ApplicationRecord
+  validates_presence_of :name
+  validates_presence_of :years_experience
   has_many :ride_mechanics
   has_many :rides, through: :ride_mechanics
   
