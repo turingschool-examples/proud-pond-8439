bb5f78cc1ebf28a7987537aac23e9121f8efa712
Johnny Bowman <johntbowman.production@gmail.com>
2022-04-11 09:28:48 -0600

    Model: mechanic relationships, create mechanic_ride model

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe Mechanic do
   describe "relationships" do
     it {should have_many(:mechanic_rides)}
-    it {should have_many(:mechanics)}
+    it {should have_many(:rides)}
   end
 end
