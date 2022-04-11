90c759518025d0b9066343efb1c3a40a283e90ff
Sherman Au <shermau13@gmail.com>
2022-04-11 09:32:20 -0600

    feat: migrate mechanics into schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_03_142730) do
+ActiveRecord::Schema.define(version: 2022_04_11_151601) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2022_01_03_142730) do
   create_table "amusement_parks", force: :cascade do |t|
     t.string "name"
     t.integer "admission_cost"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "mechanics", force: :cascade do |t|
+    t.string "name"
+    t.integer "years_of_experience"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
