67721c2fe8c82f505d6d9861891aaccc37f1cbe5
Will Medders <wmedders@gmail.com>
2022-04-11 12:06:40 -0400

    Test: mechanics index displays avg years experience of all mechanics

--- spec/features/mechnics/index_spec.rb
+++ spec/features/mechnics/index_spec.rb
@@ -26,7 +26,16 @@ RSpec.describe 'mechanics index' do
       expect(page).to have_content("Joey Simpson")
       expect(page).to have_content("5")
       expect(page).to have_no_content("Dolly Jackson")
-
     end
+  end
+
+  it 'displays avg years of experience' do
+    mechanic_1 = Mechanic.create!(name: "Joey Simpson", years_experience: 5)
+    mechanic_2 = Mechanic.create!(name: "Dolly Jackson", years_experience: 16)
+    mechanic_3 = Mechanic.create!(name: "Mac Berger", years_experience: 12)
+    mechanic_4 = Mechanic.create!(name: "Red Valance", years_experience: 24)
+    visit '/mechanics'
+    # save_and_open_page
+    expect(page).to have_content("14.25")
   end
 end
